713089dea1b41e013afaddef0c394af619952f88
alex-drocks <alexandre_d_7@hotmail.com>
2021-01-05 18:25:44 -0500

    restructured project and github jobs scraper is working nicely

---
+++ db/DB.js
@@ -0,0 +1,42 @@
+//DB.js wrapper class to use Google Sheets Spreadsheet Document as a DB
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
+module.exports = class DB {
+  doc = undefined; // Will be a Google Spreadsheet Document that may contain multiple sheets
+  isLoaded = false;
+
+  constructor(isDevMode = true) {
+    // Instantiates the DB once with the unique Google Spreadsheet document ID.
+    // In develop mode, use a document that contains only fake data
+    const { googleSheetsIDs } = require("./.configs");
+    this.doc = new GoogleSpreadsheet(googleSheetsIDs[isDevMode ? "devSheetID" : "prodSheetID"]);
+  }
+
+  async load() {
+    if (!this.doc) {
+      throw new Error("doc is not defined but it should be at this point");
+    }
+
+    // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+    const { client_email, private_key } = require("./.configs").gcpCredentials;
+    await this.doc.useServiceAccountAuth({ client_email, private_key });
+
+    await this.doc.loadInfo();
+    console.log("Loaded Google Spreadsheet Document titled: " + this.doc.title);
+
+    this.isLoaded = true;
+  }
+
+  getSheetByTitle(sheetTitle) {
+    if (!this.doc) {
+      throw new Error("this.doc is not defined");
+    } else if (!sheetTitle) {
+      throw new Error("sheetTitle is not defined");
+    } else if (!this.isLoaded) {
+      throw new Error("the DB must be loaded before getting a specific google sheet inside of the doc")
+    }
+
+    // returns a Google sheet object that must exist in the current document
+    return this.doc.sheetsByTitle[sheetTitle];
+  }
+};
---
+++ github-jobs/index.js
@@ -0,0 +1,33 @@
+//index.js
+const DB = require("../db/DB");
+const fetch = require("node-fetch");
+
+(async function () {
+  const isDevMode = true;
+
+  const json = await fetchJSON("https://jobs.github.com/positions.json?description=javascript&location=remote")
+
+  const regex = new RegExp("front", "i");
+  const rows = json
+    .filter(job => regex.test(job.title))
+    .map(job => {
+      return {
+        "COMPANY": job.company,
+        "JOB TITLE": job.title,
+        "LOCATION": job.location,
+        "DATE": job.created_at,
+        "URL": job.url,
+      }
+    });
+
+  const db = new DB(isDevMode);
+  await db.load();
+  const sheet = db.getSheetByTitle("GitHubJobs");
+
+  await sheet.addRows(rows);
+})();
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  return Array.from(await res.json());
+}
